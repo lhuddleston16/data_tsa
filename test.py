695b1db99c1103a1ff0b475565befc8f32ba40d6
Greg Wilson <greg.wilson@slalom.com>
2019-06-07 15:44:33 -0700

    updated prof

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 from data_tsa.test_data import TestData
 from data_tsa.profiler import Profiler
-from data_tsa.dataframe_inspector import DataFrameInspector
 
 test_data = TestData(100)
 df = test_data.get_test_data()
