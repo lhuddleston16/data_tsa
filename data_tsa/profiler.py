0cab8c0f0ebea980147b41f5bea3485ef190ee12
Greg Wilson <greg.wilson@slalom.com>
2019-06-11 10:26:54 -0700

    added slicer; finalized profiler output

--- data_tsa/profiler.py
+++ data_tsa/profiler.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+from pandas import DataFrame
 from data_tsa.inspector import Inspector
 from data_tsa.dataframe_inspector import DataFrameInspector
 from data_tsa.number_inspector import NumberInspector, number_dtypes
@@ -9,13 +10,20 @@ from data_tsa.date_inspector import DateInspector
 
 class Profiler:
 
-    def __init__(self, dataframe):
+    def __init__(self, dataframe, slicer=None):
         '''Profiles the columns of a pandas.DataFrame.
 
         Args:
             dataframe (pandas.DataFrame): A pands DataFrame.
+            slicer (str): Indicates a column containing logical ordered
+                partition. When specified, the profiler will profile
+                each partition in order and return the concatenated result
+                set.
         '''
         self.dataframe = dataframe
+        if slicer:
+            self.validate_column(slicer)
+        self.slicer = slicer
         self.type_exceptions = []
 
     def set_type_exception(self, column, dtype):
@@ -80,33 +88,63 @@ class Profiler:
         else:
             return ValueError('\'{}\' did not resolve to a type of \'string\', \'datetime\', or \'number\''.format(column))
 
+    def get_slicer_values(self):
+        s = self.dataframe[self.slicer].unique().tolist()
+        s.sort()
+        return s
+
+    def insp_dict_to_dataframe(self, column, inspection_dict):
+        d = {k: [v] for k, v in inspection_dict.items()}
+        df = DataFrame(d).transpose().reset_index()
+        df.columns = ['measure', 'measure_value']
+        df['column'] = column
+        return df[['column', 'measure', 'measure_value']]
+
     def profile(self):
-        '''Executes profile of provided DataFrame.
+        if self.slicer:
+            slices = self.get_slicer_values()
+        else:
+            return self.profile_dataframe(self.dataframe, None)
+
+        result = DataFrame()
+
+        for s in slices:
+            df = self.dataframe[self.dataframe[self.slicer]==s]
+            result = result.append(self.profile_dataframe(df, s))
+
+        result = result.sort_values(['inspector',
+                                     'column',
+                                     'measure',
+                                     'slice'])
+        return result
+
+    def profile_dataframe(self, dataframe, slice_value):
         '''
-        print('profiling dataframe...')
-        print('Exceptions:')
-        for _ in self.type_exceptions:
-            print('\t{} ({})'.format(_[0], _[1]))
-
-        print('Column dtypes:')
-        for col in self.dataframe.columns:
-            print('\t{} ({})'.format(col, self.get_column_dtype(col)))
-
-        print('Duplicate rows detected:')
-        df_insp = DataFrameInspector(self.dataframe)
-        print('\t', df_insp.get_duplicate_row_indicator())
-
-        print('Profiling columns:')
-        for col in self.dataframe.columns:
-            print('-', col)
+        '''
+        output_columns = ['inspector',
+                          'column',
+                          'slice',
+                          'measure',
+                          'measure_value']
+        result = DataFrame()
+        for col in dataframe.columns:
             dtype = self.get_column_dtype(col)
             if dtype == 'string':
                 insp = StringInspector(self.dataframe[col])
+                inspector_type = 'string'
             elif dtype == 'number':
                 insp = NumberInspector(self.dataframe[col])
+                inspector_type = 'number'
             elif dtype == 'datetime':
                 insp = DateInspector(self.dataframe[col])
+                inspector_type = 'datetime'
             else:
                 insp = Inspector(self.dataframe[col])
-            for k, v in insp.inspect().items():
-                print('\t', k, '>', v)
+                inspector_type = 'generic'
+            insp_dict = insp.inspect()
+            df = self.insp_dict_to_dataframe(col, insp_dict)
+            df['inspector'] = inspector_type
+            df['slice'] = slice_value
+            result = result.append(df[output_columns])
+        return result
+
--- examples.py
+++ examples.py
@@ -1,9 +1,28 @@
-from data_tsa.test_data import TestData
-from data_tsa import Profiler
+from data_tsa.sample_data import SampleData
+from data_tsa.profiler import Profiler
 
-test_data = TestData(100)
-df = test_data.get_test_data()
+# Generate test dat
+samp = SampleData(1000)
+df = samp.get_sample_data()
 
+# Initialize Profiler with no slicer
 profiler = Profiler(df)
-#profiler.set_type_exception('created_at', 'date')
-profiler.profile()
+
+# Profile dataframe columns
+p1_output = profiler.profile()
+p1_output.head(20)
+
+# Initialize Profiler with slcer
+profiler_slicer = Profiler(df, slicer='string_slicer')
+
+# Profile dataframe columns over slicer partitions
+p2_output = profiler_slicer.profile()
+p2_output.head(20)
+
+# Check dataframe for row duplicates
+from data_tsa.dataframe_inspector import DataFrameInspector
+from pandas import DataFrame
+
+df_dupes = DataFrame({'a': [0, 0], 'b': [0, 0]})
+insp = DataFrameInspector(df_dupes)
+insp.get_duplicate_row_indicator()
